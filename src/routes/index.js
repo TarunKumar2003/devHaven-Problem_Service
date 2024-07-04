286815373f8d326070c01c3b071693dbe90b344f
Tarun Kumar <tarunnimbal123@gmail.com>
2024-07-04 15:27:26 +0530

    Small change in v1Router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
-const v1Router = require('./v1')
+const v1Router = require("./v1");
 
 const apiRouter = express.Router();
 
-apiRouter.use('/v1', v1Router);
+apiRouter.use("/v1", v1Router);
 
 module.exports = apiRouter;
