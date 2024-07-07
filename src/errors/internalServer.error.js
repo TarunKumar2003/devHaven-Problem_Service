1da8e59828d8228b83fedb2115cb6274b5a6de0b
Tarun Kumar <tarunnimbal123@gmail.com>
2024-07-07 15:25:33 +0530

    Small Changes in Problem Controller and internalServerError File

--- src/errors/internalServer.error.js
+++ src/errors/internalServer.error.js
@@ -1,11 +1,15 @@
 const { StatusCodes } = require("http-status-codes");
 const BaseError = require("./base.error");
 
-
-class InternalServerError extends BaseError{
-    constructor(details){
-        super("Internal Server Error", StatusCodes.InternalServerError, "Something went wrong !!", details);
-    }
+class InternalServerError extends BaseError {
+  constructor(details) {
+    super(
+      "Internal Server Error",
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      "Something went wrong !!",
+      details
+    );
+  }
 }
 
 module.exports = InternalServerError;
