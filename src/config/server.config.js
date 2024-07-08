3c8565f6d9148bca2edb1fad202c7b0e6cadb0e2
Tarun Kumar <tarunnimbal123@gmail.com>
2024-07-08 20:23:40 +0530

    Add enviroment variable

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-    PORT:process.env.PORT,
-    MONGODB_URI:process.env.MONGODB_URI
-}
-
+  PORT: process.env.PORT,
+  MONGODB_URI: process.env.MONGODB_URI,
+  NODE_ENV: process.env.NODE_ENV || "development",
+};
